6e3a2daf8d2fbb463388a3441fdfb35177f67925
Sergio Robledo Sarrión <s.robledo.2021@alumnos.urjc.es>
2023-09-25 10:13:19 +0200

    update

--- ej1.py
+++ ej1.py
@@ -4,8 +4,34 @@ import time
 import random
 # Enter sequential code!
 
+SPIRAL=1
+BACKWARDS=2
+TURN=3
+FORWARD=4
+
 state= 1
 radius=0.3
+
+def bumper_hit():
+  bumper_value=HAL.getBumperData().bumper
+  #print(bumper_value)
+  if bumper_value == 0:
+    print("izq")
+    HAL.setW(1)
+    HAL.setV(0)
+    return "hit"
+  if bumper_value == 1:
+    print("centro")
+    HAL.setV(-1)
+    HAL.setW(0)
+    return "hit"
+  if bumper_value == 2:
+    print("dcha")
+    HAL.setV(-1)
+    HAL.setW(0)
+    return "hit"
+  time.sleep(0.5)
+  
 
 def spiral(radius):
   bumper_value=HAL.getBumperData().state
@@ -22,6 +48,9 @@ def spiral(radius):
   
 def backwards():
   print("Im Michael Jackson dude!")
+  HAL.setV(-1)
+  time.sleep(0.5)
+  return "turn"
   
 def turn():
   print("turn up the music!")
@@ -42,26 +71,27 @@ def forward():
     print("entra bumper1")
     return "spiral"
   else:
-    time.sleep(5)
+    time.sleep(7)
     return "spiral"
   
 while True:
     print(state)
-    if state == 1:
+    if state == SPIRAL:
       radius=radius+0.1
       if spiral(radius) == "turn":
-        state=3
+        state=TURN
         #if the vel. is too high, reset and go forward
       if radius >= 1.4:
         state=4
       
-    if state == 2:
-      backwards()
-    if state == 3:
+    if state == BACKWARDS:
+      if backwards() == "turn":
+        state=TURN
+    if state == TURN:
       if turn() == "forward":
-        state=4
-    if state == 4:
+        state=FORWARD
+    if state == FORWARD:
       if forward() == "spiral":
         #reset velocity
         radius=0.5
-        state=1
+        state=SPIRAL
