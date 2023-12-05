18dd72de6f29ab6e19000e0e3e8391dd8de2c506
srobledo2021 <113594786+srobledo2021@users.noreply.github.com>
2023-12-05 23:09:55 +0100

    Update ej4.py

--- ej4.py
+++ ej4.py
@@ -10,6 +10,7 @@ import time
 map_height= 400
 map_width= 400
 
+DISTANCE = 1
 
 def gridToWorld(map_cell):
   world_x = map_cell[1] * 500 / map_height -250
@@ -21,103 +22,47 @@ def normalize_grid(grid):
     max_grid = np.max(grid)
     return np.clip(grid * 255 / max_grid, 0, 255).astype('uint8')
   
-def bfs_search(map_array, target_map, start_pos):
-
-    priority_queue = queue.PriorityQueue()
-    visited_positions = set()
-    visited_positions.add(target_map)
-
-    #Insert Target Node into the priority queue
-    priority_queue.put((0, target_map))
-
-    while not priority_queue.empty(): 
-        #Pop node from priority queue
-        cost, current = priority_queue.get()
-        #Atart node End
-        if current == start_pos:
-            break
-
-        # Whenever you find an obstacle
-        if map_array[current[1], current[0]] == 0:  
-            # obstacle value = 0
-            continue
-
-        # Assign weight to neighbors of c if previously unassigned
-        grid[current[1], current[0]] = cost
-
-
-        # Get neighbors and insert them
-        neighbors = [
-            (current[0], current[1] - 1),  # U
-            (current[0], current[1] + 1),  # D
-            (current[0] - 1, current[1]),  # L
-            (current[0] + 1, current[1]),  # R
-            (current[0] - 1, current[1] - 1),  # UL
-            (current[0] - 1, current[1] + 1),  # DL
-            (current[0] + 1, current[1] - 1),  # UR
-            (current[0] + 1, current[1] + 1),  # DR
-        ]
-
-        for neighbor in neighbors:
-            if (neighbor[0] < 0 or neighbor[0] >= grid.shape[1] or neighbor[1] < 0 or neighbor[1] >= grid.shape[0]):
-                continue
-
-            # Already visited neighbor?
-            if neighbor in visited_positions:
-                continue
-
-            # Grid with costs
-            if neighbor[0] == current[0] or neighbor[1] == current[1]:
-                grid[neighbor[1], neighbor[0]] = cost + 1
-            else:
-                grid[neighbor[1], neighbor[0]] = cost + math.sqrt(2)
-
-            # Update priority queue
-            priority_queue.put((grid[neighbor[1], neighbor[0]], neighbor))
-            # Mark as visited
-            visited_positions.add(neighbor)
-    return grid
 
 def get_path_coords(grid, start_pos):
     path = []
-    current = start_pos
-    while (grid[current[1], current[0]] != 0):
-        path.append(current)
+    current_coord = start_pos
+    while (grid[current_coord[1], current_coord[0]] != 0):
+        path.append(current_coord)
 
         neighbors = [
-            (current[0], current[1] - 1),  # U
-            (current[0], current[1] + 1),  # D
-            (current[0] - 1, current[1]),  # L
-            (current[0] + 1, current[1]),  # R
-            (current[0] - 1, current[1] - 1),  # UL
-            (current[0] - 1, current[1] + 1),  # DL
-            (current[0] + 1, current[1] - 1),  # UR
-            (current[0] + 1, current[1] + 1),  # DR
+            (current_coord[0], current_coord[1] - 1),  # U
+            (current_coord[0], current_coord[1] + 1),  # D
+            (current_coord[0] - 1, current_coord[1]),  # L
+            (current_coord[0] + 1, current_coord[1]),  # R
+            (current_coord[0] - 1, current_coord[1] - 1),  # UL
+            (current_coord[0] - 1, current_coord[1] + 1),  # DL
+            (current_coord[0] + 1, current_coord[1] - 1),  # UR
+            (current_coord[0] + 1, current_coord[1] + 1),  # DR
         ]
 
-        min_cost = float("inf")
         next_pos = None
+        less_cost = float("inf")
 
         for neighbor in neighbors:
-            if (0 <= neighbor[0] < grid.shape[1] and 0 <= neighbor[1] < grid.shape[0] and grid[neighbor[1], neighbor[0]] < min_cost ):
-                min_cost = grid[neighbor[1], neighbor[0]]
+            if (0 <= neighbor[0] < grid.shape[1] and 0 <= neighbor[1] < grid.shape[0] and grid[neighbor[1], neighbor[0]] < less_cost ):
+                less_cost = grid[neighbor[1], neighbor[0]]
                 next_pos = neighbor
 
 
         if next_pos is not None:
-            current = next_pos
-        else:
-            print("Error: Could not find path")
-            break
+            current_coord = next_pos
+        #else:
+        #    print("Error: Could not find path")
+        #    break
 
-    path.append(current)
+    path.append(current_coord)
     return path
 
 
 def path_into_vectors(full_path,goal):
 
-    result_vectors  = []
     current_vector = [full_path[0]]
+    result_vectors  = []
     for i in range(1, len(full_path)):
       if i == len(full_path) - 1:
           next_grid = full_path[i]
@@ -136,21 +81,21 @@ def path_into_vectors(full_path,goal):
             current_vector.append(grid_on)
             continue
         
-        
-        #UP-RIGHT
-        if (grid_on[0] + grid_on[1] == prev_grid[0] + prev_grid[1]) and \
-                (grid_on[0] + grid_on[1] != next_grid[0] + next_grid[1]):
-            current_vector.append(grid_on)
-            continue
-
         # UP-Left
         if (grid_on[0] - grid_on[1] == prev_grid[0] - prev_grid[1]) and \
                 (grid_on[0] - grid_on[1] != next_grid[0] - next_grid[1]):
             current_vector.append(grid_on)
             continue
 
+        #UP-RIGHT
+        if (grid_on[0] + grid_on[1] == prev_grid[0] + prev_grid[1]) and \
+                (grid_on[0] + grid_on[1] != next_grid[0] + next_grid[1]):
+            current_vector.append(grid_on)
+            continue
+
         else:
             continue
+
     result_vectors.append(current_vector)
     result_vectors.append([goal])
     return result_vectors
@@ -162,6 +107,7 @@ def orientate(x_rel, y_rel):
     angle = gridToWorld(pos)
     car_yaw = HAL.getPose3d().yaw 
 
+    #get car and goal angles
     atan_val = math.atan2(angle[0] - HAL.getPose3d().x , angle[1]- HAL.getPose3d().y)
     if  atan_val <= 0:
       atan_val += 2 * math.pi
@@ -173,6 +119,7 @@ def orientate(x_rel, y_rel):
     car_angl = math.degrees(car_yaw)
     goal_ang = (math.degrees(atan_val) - 90)
 
+    #difference in angles (face objective)
     while abs(car_angl - goal_ang) > 0.033:
       car_yaw = HAL.getPose3d().yaw 
       if car_yaw < 0:
@@ -193,13 +140,70 @@ def move_forward(x_rel, y_rel):
     xdist = abs(x_rel - car_pos[0])
     ydist = abs(y_rel - car_pos[1])
     manhattan_dist = math.sqrt((xdist**2) + (ydist**2))
-    if ((manhattan_dist) > 1):
+    if ((manhattan_dist) > DISTANCE):
       HAL.setV(manhattan_dist * 0.4)
     else:
       HAL.setV(0)
       break
 
   return
+
+def bfs_search(map_array, target_map, start_pos):
+
+    priority_queue = queue.PriorityQueue()
+    visited_positions = set()
+    visited_positions.add(target_map)
+
+    #Insert Target Node into the priority queue
+    priority_queue.put((0, target_map))
+
+    while not priority_queue.empty(): 
+        #Pop node from priority queue
+        cost, current_node = priority_queue.get()
+        #Atart node End
+        if current_node == start_pos:
+            break
+
+        # Whenever you find an obstacle
+        if map_array[current_node[1], current_node[0]] == 0:  
+            # obstacle value = 0
+            continue
+
+        # Assign weight to neighbors of c if previously unassigned
+        grid[current_node[1], current_node[0]] = cost
+
+        # Get neighbors and insert them
+        neighbors = [
+            (current_node[0], current_node[1] - 1),  # U
+            (current_node[0], current_node[1] + 1),  # D
+            (current_node[0] - 1, current_node[1]),  # L
+            (current_node[0] + 1, current_node[1]),  # R
+            (current_node[0] - 1, current_node[1] - 1),  # UL
+            (current_node[0] - 1, current_node[1] + 1),  # DL
+            (current_node[0] + 1, current_node[1] - 1),  # UR
+            (current_node[0] + 1, current_node[1] + 1),  # DR
+        ]
+
+        for neighbor in neighbors:
+            if (neighbor[0] < 0 or neighbor[0] >= grid.shape[1] or neighbor[1] < 0 or neighbor[1] >= grid.shape[0]):
+                continue
+
+            # Already visited neighbor?
+            if neighbor in visited_positions:
+                continue
+
+            # Grid with costs
+            if neighbor[0] == current_node[0] or neighbor[1] == current_node[1]:
+                grid[neighbor[1], neighbor[0]] = cost + 1
+            else:
+                grid[neighbor[1], neighbor[0]] = cost + math.sqrt(2)
+
+            # Update priority queue
+            priority_queue.put((grid[neighbor[1], neighbor[0]], neighbor))
+            # Mark as visited
+            visited_positions.add(neighbor)
+    return grid
+
 
 def check_reached_goal(result_path):
     if i == len(result_path) - 1:
@@ -224,7 +228,6 @@ goal_pose  = GUI.getTargetPose()
 new_target_map = tuple(MAP.rowColumn(goal_pose))
 
  
-
 while True:
     
     #get map with costs using BFS search algorythm
