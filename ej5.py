b6e3d9556ca982a83ab3f1d72dd7b16fc1841fbb
srobledo2021 <113594786+srobledo2021@users.noreply.github.com>
2023-12-10 20:35:14 +0100

    Update ej5.py

--- ej5.py
+++ ej5.py
@@ -1,62 +1,48 @@
 from GUI import GUI
 from HAL import HAL
 import random
+import math
+
+
+num_particles = 1000
 
 # Enter sequential code!
-
-particle_filter = ParticleFilter(num_particles=100)
-self.num_particles = num_particles
-self.particles = self.initialize_particles()
-
-def initialize_particles(self):
+def initialize_particles():
     # Initialize particles with random positions and equal weights
-    particles = [[random.uniform(0, 100), random.uniform(0, 100), random.uniform(0, 2 * 3.1416), 1.0] 
-                  for _ in range(self.num_particles)]
+    particles = [[random.uniform(0, 400), random.uniform(0, 400), random.uniform(0, 2 * math.pi), 1.0] 
+                  for _ in range(num_particles)]
     return particles
-
-def move_particles(self, distance, sensor_data):
-    for i in range(self.num_particles):
-        # Move the particle
-        self.move_particle(i, distance)
-        
-        # Update the particle's weight based on sensor data
-        self.update_particle_weight(i, sensor_data)
     
-    # Resample particles based on weights
-    self.resample_particles()
+def move_particles(particles, distance):
+    for particle in particles:
+        move_particle(particle, distance)
+    
+    
+def move_particle(particle, distance):
+    # Implement a simple motion model for particle movement
+    angle_noise = random.uniform(-0.1, 0.1)
+    distance_noise = random.uniform(-0.1, 0.1)
 
-def move_particle(self, index, distance):
-    # Implement your motion model here based on the robot's movement
-    # You can use HAL.setV() and HAL.setW() to control the robot's motion
-    # Update the particle's position (x, y, angle) accordingly
+    particle[0] += distance * math.cos(particle[2]) + distance_noise
+    particle[1] += distance * math.sin(particle[2]) + distance_noise
+    particle[2] += angle_noise
 
-def update_particle_weight(self, index, sensor_data):
-    # Implement the observation model here based on sensor data
-    # You can use HAL.getImage() and other relevant functions
-    # Update the particle's weight based on the observation model
+    # Ensure angles are within [0, 2 * pi)
+    particle[2] %= 2 * math.pi
 
-def resample_particles(self):
-    # Resample particles based on their weights
-    weights = [particle[3] for particle in self.particles]
-    new_particles = random.choices(self.particles, weights=weights, k=self.num_particles)
-    self.particles = new_particles
-
+particles = initialize_particles()
 
 
 # Main loop
 while True:
-        # Move the robot and read sensor data
-        HAL.setV(1.0)  # Set linear speed
-        HAL.setW(0.0)  # Set angular velocity
-        distance = 1.0  # Example distance moved by the robot
-        sensor_data = HAL.getImage()  # Example sensor data
-        
-        # Perform particle filter update
-        particle_filter.move_particles(distance, sensor_data)
-        
-        # Display particles on the GUI
-        GUI.showParticles(particle_filter.particles)
+    # Move the robot and read sensor data
+    HAL.setV(1.0)  # Set linear speed
+    HAL.setW(0.0)  # Set angular velocity
+    distance = 1.0  # Example distance moved by the robot
+    sensor_data = HAL.getImage()  # Example sensor data
     
-    # Cleanup
-    HAL.close()
+    move_particles(particles, distance)
+    # Display particles on the GUI
+    GUI.showParticles(particles)
+
     
