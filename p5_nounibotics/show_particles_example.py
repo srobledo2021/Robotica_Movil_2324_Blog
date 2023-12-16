1dc2a9d99963ee4fdc3db7ad501df31a1b93484d
Sergio Robledo Sarrión <s.robledo.2021@alumnos.urjc.es>
2023-12-16 19:07:22 +0100

    p5 code update

--- p5_nounibotics/show_particles_example.py
+++ p5_nounibotics/show_particles_example.py
@@ -1,10 +1,13 @@
 import numpy as np
 from GUI import GUI
+from HAL import HAL
 import MAP
+import time
 
-N_PARTICLES = 100
+N_PARTICLES = 300
 
 map = MAP.getMap()
+
 
 def initialize_particles():
     """ Generate random particles in world coordinates (meters).
@@ -31,21 +34,129 @@ def initialize_particles():
 
     return particles
 
+def simulate_lasers(particle, map):
+    """ Simula los láseres virtuales para una partícula en el mapa."""
+    x, y, yaw = particle[:3]
+    laser_angles = np.linspace(-np.pi / 2, np.pi / 2, num=180)  # Ángulos del láser
+    laser_distances = []
 
+    for angle in laser_angles:
+        dx = np.cos(yaw + angle)
+        dy = np.sin(yaw + angle)
+        distance = 0.0
+
+        while True:
+            distance += 0.1  # Incremento en la distancia
+            map_x, map_y, map_yaw = MAP.worldToMap(x + distance * dx, y + distance * dy, yaw)
+            
+            if not (0 <= map_x < map.shape[1] and 0 <= map_y < map.shape[0]):
+                break  # Fuera de los límites del mapa
+
+            if map[map_y, map_x] == 1:  # Si es un obstáculo
+                break
+
+        laser_distances.append(distance)
+
+    return np.array(laser_distances)
+
+def compute_particle_weights(particles, robot_laser_data, map):
+    """ Calcula los pesos de las partículas basándose en la similitud con los datos del láser del robot."""
+    weights = []
+
+    for particle in particles:
+        particle_laser_data = simulate_lasers(particle, map)
+        similarity = np.exp(-np.linalg.norm(particle_laser_data - robot_laser_data))
+        weights.append(similarity)
+
+    weights = np.array(weights)
+    weights /= np.sum(weights)  # Normalizar pesos para que sumen 1
+
+    return weights
+
+def resample_particles(particles, weights):
+    """ Resamplea las partículas basándose en sus pesos. """
+    indices = np.random.choice(np.arange(len(particles)), size=N_PARTICLES, replace=True, p=weights)
+    return particles[indices]
+
+
+#Ahora lo que queda por hacer es ver a donde apunta con el
+# laser cada particula y comparar esto con los datos del 
+# robot de su laser. Los que mas se asemejen se mantiene y los que no se descartan.
+# Se usa lo de dda para los láseres virtuales.
 def main():
+    #START
+    #-------------Set of particles--------------------------------------------
     # Create the GUI object
     gui = GUI()
-    
-    # Initialize some random particles
+    # Initialize random particles
     particles = initialize_particles()
-    print(F"Particles:\n{particles}")
+    #print(F"Particles:\n{particles}")
    
-    #Ahora lo que queda por hacer es ver a donde apunta con el laser cada particula y comparar esto con los datos del robot de su laser. Los que mas se asemejen se mantieney los que no se descartan. SE usa lo de dda para los láseres virtuales.
-    # Show the particles in the GUI
-    gui.showParticles(particles [:,:3])
+    #---------Robot moves----------------------------------------------------------------------
+    # Create a HAL (robot) object
+    robot = HAL()
+    # Set a custom initial pose
+    robot.pose[0] = 1.1
+    # Create a GUI object and link it with the robot
+    gui = GUI(robot=robot)
+    print(F"Robot pose: {gui.getRobotPose()}")
 
-    # Update the GUI image and wait
-    gui.updateGUI(block=True)
+    # Set a small velocity
+    robot.setV(0.3)
+    robot.setW(0.8)
+    
 
+    while True:
+        # Get some laser data and show it in the GUI
+        robot_laser_data = robot.getLaserData()
+        gui.showLaser(robot_laser_data)
+
+        # Robot pose is automatically updated inside the GUI "updateGUI"
+        # robot.updatePose()
+        gui.updateGUI()
+        time.sleep(0.1)
+
+
+        #----------Particles mimic movement---------------------------------------------
+    
+    
+        #------------Particle weight-----------------------------------
+        
+        car_yaw = gui.getRobotPose()[2]
+        print(car_yaw)
+        coincidence_particles= []
+        '''
+        for particle in particles:
+            x, y, yaw = particle[:3]
+            #print(F"Laser X, Y, yaw:\n{x,y,yaw}")
+            if ((car_yaw <= 0.5 + yaw) and (car_yaw >= yaw - 0.5)):
+                coincidence_particles.append(particle)
+                print("DENTROOO")
+                print(particle)
+        
+        # Iterar sobre el tiempo
+        for _ in range(10):  # Puedes ajustar el número de iteraciones según tus necesidades
+            # Calcular pesos de las partículas
+            weights = compute_particle_weights(particles, robot_laser_data, map)
+
+            # Resamplear partículas
+            particles = resample_particles(particles, weights)
+
+            # Actualizar la GUI y esperar
+            gui.updateGUI(block=True)
+
+
+        gui.showParticles(particles[:, :3],weights)
+        '''
+
+        #-------------LAST THING TO DO IS JUST PRINT------------------------------------------------------------------
+        
+        # Show the particles in the GUI
+        gui.showParticles(particles [:,:3])
+
+        # Update the GUI image and wait
+        #gui.updateGUI(block=True)
+
+    
 if __name__ == '__main__':
     main()
