e3b344820fe644e11ae425f50abbccc8e3368c4a
Sergio Robledo Sarrión <s.robledo.2021@alumnos.urjc.es>
2023-12-16 20:44:02 +0100

    ovement achieved

--- p5_nounibotics/show_particles_example.py
+++ p5_nounibotics/show_particles_example.py
@@ -4,7 +4,7 @@ from HAL import HAL
 import MAP
 import time
 
-N_PARTICLES = 300
+N_PARTICLES = 3
 
 map = MAP.getMap()
 
@@ -34,8 +34,25 @@ def initialize_particles():
 
     return particles
 
+def dda_algorithm(x0, y0, x1, y1):
+    """ DDA algorithm for line drawing between two points (x0, y0) and (x1, y1)."""
+    dx = x1 - x0
+    dy = y1 - y0
+    steps = max(abs(dx), abs(dy))
+    x_increment = dx / steps
+    y_increment = dy / steps
+    x, y = x0, y0
+    points = []
+
+    for _ in range(steps):
+        points.append((int(round(x)), int(round(y))))
+        x += x_increment
+        y += y_increment
+
+    return points
+
 def simulate_lasers(particle, map):
-    """ Simula los láseres virtuales para una partícula en el mapa."""
+    """ Simula los láseres virtuales para una partícula en el mapa usando el algoritmo DDA."""
     x, y, yaw = particle[:3]
     laser_angles = np.linspace(-np.pi / 2, np.pi / 2, num=180)  # Ángulos del láser
     laser_distances = []
@@ -43,21 +60,27 @@ def simulate_lasers(particle, map):
     for angle in laser_angles:
         dx = np.cos(yaw + angle)
         dy = np.sin(yaw + angle)
-        distance = 0.0
 
-        while True:
-            distance += 0.1  # Incremento en la distancia
-            map_x, map_y, map_yaw = MAP.worldToMap(x + distance * dx, y + distance * dy, yaw)
-            
-            if not (0 <= map_x < map.shape[1] and 0 <= map_y < map.shape[0]):
+        start_x, start_y, _ = MAP.worldToMap(x, y, yaw)
+        end_x = int(round(x + 10 * dx))  # Extend the ray by 10 units
+        end_y = int(round(y + 10 * dy))
+
+        points = dda_algorithm(start_x, start_y, end_x, end_y)
+
+        for point in points:
+            if not (0 <= point[0] < map.shape[1] and 0 <= point[1] < map.shape[0]):
                 break  # Fuera de los límites del mapa
 
-            if map[map_y, map_x] == 1:  # Si es un obstáculo
+            if map[point[1], point[0]] == 1:  # Si es un obstáculo
+                distance = np.sqrt((point[0] - start_x)**2 + (point[1] - start_y)**2)
+                laser_distances.append(distance)
                 break
 
-        laser_distances.append(distance)
-
     return np.array(laser_distances)
+
+# Rest of the code remains unchanged
+
+
 
 def compute_particle_weights(particles, robot_laser_data, map):
     """ Calcula los pesos de las partículas basándose en la similitud con los datos del láser del robot."""
@@ -65,13 +88,26 @@ def compute_particle_weights(particles, robot_laser_data, map):
 
     for particle in particles:
         particle_laser_data = simulate_lasers(particle, map)
-        similarity = np.exp(-np.linalg.norm(particle_laser_data - robot_laser_data))
-        weights.append(similarity)
+        print(robot_laser_data)
+        print(particle_laser_data)
+        # Ensure that both particle_laser_data and robot_laser_data are non-empty
+        if len(particle_laser_data) == 0 or len(robot_laser_data) == 0:
+            similarity = 0.0
+        else:
+            # Compute cosine similarity
+            similarity = np.dot(particle_laser_data, robot_laser_data) / (
+                np.linalg.norm(particle_laser_data) * np.linalg.norm(robot_laser_data)
+            )
+
+        # Convert similarity to a weight between 0 and 1
+        weight = 0.5 + 0.5 * similarity  # Scales similarity to the range [0, 1]
+        weights.append(weight)
 
     weights = np.array(weights)
     weights /= np.sum(weights)  # Normalizar pesos para que sumen 1
 
     return weights
+
 
 def resample_particles(particles, weights):
     """ Resamplea las partículas basándose en sus pesos. """
@@ -116,16 +152,23 @@ def main():
         gui.updateGUI()
         time.sleep(0.1)
 
-
         #----------Particles mimic movement---------------------------------------------
-    
-    
+        # Move the particles to mimic the movement of the robot
+        for i in range(N_PARTICLES):
+            v = 0.3 
+            w = 0.8 
+
+            particles[i, 0] += v * np.cos(particles[i, 2])
+            particles[i, 1] += v * np.sin(particles[i, 2])
+            particles[i, 2] += w
+
+
         #------------Particle weight-----------------------------------
-        
+        '''
         car_yaw = gui.getRobotPose()[2]
         print(car_yaw)
         coincidence_particles= []
-        '''
+        
         for particle in particles:
             x, y, yaw = particle[:3]
             #print(F"Laser X, Y, yaw:\n{x,y,yaw}")
@@ -148,7 +191,12 @@ def main():
 
         gui.showParticles(particles[:, :3],weights)
         '''
+        # Compute particle weights based on similarity to robot's laser data
+        weights = compute_particle_weights(particles, robot_laser_data, map)
 
+        # Resample particles based on their weights
+        #particles = resample_particles(particles, weights)
+    
         #-------------LAST THING TO DO IS JUST PRINT------------------------------------------------------------------
         
         # Show the particles in the GUI
